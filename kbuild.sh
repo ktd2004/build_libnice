d5f8c4ea98ff485d6a7d72c827f99b5bb9d953a6
TaeDong Kim <fly1004@gmail.com>
2018-01-04 19:40:20 +0900

    gnutls 빌드 중

--- kbuild.sh
+++ kbuild.sh
@@ -16,7 +16,6 @@
 export CUR="`pwd`"
 export OUTPUT="${CUR}/output/"
 
-export CC=arm-hisiv400-linux-gcc
 
 export HOSTOPT="--host=arm-hisiv400-linux"
 export PREFIXOPT="--prefix=${OUTPUT}"
@@ -32,6 +31,7 @@ fi
 tar xvfj gmp-6.1.2.tar.bz2
 
 cd gmp-6.1.2
+export CC=arm-hisiv400-linux-gcc
 ./configure ${HOSTOPT} ${PREFIXOPT} && make && make install
 cd $CUR
 
@@ -43,5 +43,25 @@ fi
 tar xvfz nettle-3.4.tar.gz
 
 cd nettle-3.4
+export CC=arm-hisiv400-linux-gcc
+export CFLAGS="-I${OUTPUT}/include/"
+export LDFLAGS="-I${OUTPUT}/lib/"
 ./configure ${HOSTOPT} ${PREFIXOPT} && make && make install
+cd $CUR
+
+
+
+rm -fr gnutls-3.5.16
+if [ ! -f gnutls-3.5.16.tar.xz ];then
+    wget https://www.gnupg.org/ftp/gcrypt/gnutls/v3.5/gnutls-3.5.16.tar.xz
+fi
+tar xvfJ gnutls-3.5.16.tar.xz
+
+cd gnutls-3.5.16
+export CC=arm-hisiv400-linux-gcc
+export CFLAGS="-I${OUTPUT}/include/"
+export LDFLAGS="-L${OUTPUT}/lib/"
+export LIBS="-lhogweed -lnettle -lgmp"
+./configure ${HOSTOPT} ${PREFIXOPT} --with-included-libtasn1 --with-included-unistring --without-idn --without-p11-kit
+make && make install
 cd $CUR
